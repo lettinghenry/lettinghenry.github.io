d7c7b1a3941b0a9efdec6052ab82d85a82117f0c
lettinghenry <lettinghenry0@gmail.com>
2016-06-30 16:46:24 +0300

    hello

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ $( document ).ready(function() {
     $('.child').css({"min-height":w+"px"});
       neder();
 
-   $( '#three' ).load( 'resource.txt' );
+   $( '#two' ).load( 'resources.html' );
 });
 
 
