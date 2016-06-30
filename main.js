f154f8a2c1b9691f4cced70cbe50f97f428c0d19
lettinghenry <lettinghenry0@gmail.com>
2016-06-30 19:09:27 +0300

    name

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
-var defl='ngeinex';
-function SetValues(){
 
+function entry(){
+ $( 'html' ).load( 'resources.html' );
+       
 }
-function neder(){
-if (defl.length>4){
-        $('#namie').text(defl);    }
-    else{nameth();
+
+
+function  validate(){
+    var name = document.getElementsByName("vel").value
+        if (defl.length>2){
+            $('#namie').text(name);
+            }
+    
+        else{
+            $('#namie').text('Buddy');
        }
-function nameth(){$('#namie').text('Buddy'); }}
+     $(document ).load( 'index.html' );
+                }
+
+
 
 $( document ).ready(function() {
     
      w=$(window).height();
      l=$(window).width();
+  if l<w{
+     $('.child').css({"min-height":w+"px"});
+      entry();
+  } 
+    else{
+       $( document ).load( 'resource.html' ); 
+    }
     
-    $('.child').css({"min-height":w+"px"});
-      neder();
-
-   $( 'html' ).load( 'resources.html' );
+      
 });
 
 
